9fc9214dd0e85dff7ff78c03adb76a8291b54284
DSahalatyi <dsahalatyi@gmail.com>
2024-07-18 14:07:30 +0300

    fix annotations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import math
 
 
@@ -6,19 +7,19 @@ class Vector:
         self.x = round(x_coord, 2)
         self.y = round(y_coord, 2)
 
-    def __add__(self, other: "Vector") -> "Vector":
+    def __add__(self, other: Vector) -> Vector:
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __sub__(self, other: "Vector") -> "Vector":
+    def __sub__(self, other: Vector) -> Vector:
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other: (float, "Vector")) -> (float, "Vector"):
+    def __mul__(self, other: float | Vector) -> float | Vector:
         if isinstance(other, (int, float)):
             return Vector(self.x * other, self.y * other)
         return self.x * other.x + self.y * other.y
 
     @classmethod
-    def create_vector_by_two_points(cls, start: tuple, end: tuple) -> "Vector":
+    def create_vector_by_two_points(cls, start: tuple, end: tuple) -> Vector:
         res_x = end[0] - start[0]
         res_y = end[1] - start[1]
         return cls(res_x, res_y)
@@ -26,11 +27,11 @@ class Vector:
     def get_length(self) -> float:
         return (self.x**2 + self.y**2)**0.5
 
-    def get_normalized(self) -> "Vector":
+    def get_normalized(self) -> Vector:
         magnitude = self.get_length()
         return Vector(self.x / magnitude, self.y / magnitude)
 
-    def angle_between(self, other: "Vector") -> float:
+    def angle_between(self, other: Vector) -> float:
         dot_product = self * other
         self_magnitude = self.get_length()
         other_magnitude = other.get_length()
@@ -41,7 +42,7 @@ class Vector:
         y_axis = Vector(0, 1)
         return self.angle_between(y_axis)
 
-    def rotate(self, degrees: int) -> "Vector":
+    def rotate(self, degrees: int) -> Vector:
         degrees = math.radians(degrees)
         res_x = math.cos(degrees) * self.x - math.sin(degrees) * self.y
         res_y = math.sin(degrees) * self.x + math.cos(degrees) * self.y
